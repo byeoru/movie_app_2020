5201a2d6030463a4595715cebebb45333a4da2df
byeoru <byeoru215@gmail.com>
2020-05-28 00:52:50 +0900

    Updates

---
+++ precache-manifest.d8d860119a6a822289baeb9ee391e3d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131a243c878af5ed40b64081f34ae59e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "57c8ace48a8b5b99147c",
+    "url": "/movie_app_2020/static/css/main.6ccf5d5a.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c8ace48a8b5b99147c",
+    "url": "/movie_app_2020/static/js/main.16a43eba.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
